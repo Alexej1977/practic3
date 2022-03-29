bc0719a44906c18216c4eae6de6a176310f3d716
Alexej Berezgnoj <bereznojaleksej373@gmail.com>
2022-03-29 15:03:26 +0300

    fist commit

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+
+
+const { src, dest, parallel, series, watch } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const csso = require('gulp-csso');
+const del = require('del');
+const sync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
+
+
+function scss() {
+	return src('style/scss/**.scss')
+		.pipe(sass())
+		.pipe(autoprefixer({
+			overrideBrowserslist: ["last 2 versions"]
+		}))
+		.pipe(concat("main.css"))
+		.pipe(dest('style/css/'));
+}
+
+function clear() {
+	return del('style/css/main.css');
+}
+
+function browserSync() {
+	sync.init({
+		server: './'
+	})
+
+	watch('style/scss/**.scss', series(clear, scss)).on('change', sync.reload);
+	watch('**.html', series(clear, scss)).on('change', sync.reload);
+}
+
+exports.scss = scss;
+exports.clear = clear;
+exports.build = series(clear, scss);
+exports.default = browserSync;
